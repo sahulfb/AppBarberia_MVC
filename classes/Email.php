6c71fa6623d75d6bdbe05c508a3a0e8d72fa92cb
sahulFB <sahulfb@gmail.com>
2022-06-20 11:58:05 -0400

    cambio de dominio en email y en js

--- classes/Email.php
+++ classes/Email.php
@@ -35,7 +35,7 @@ class Email{
 
         $contenido= "<html>";
         $contenido.= "<p><strong>Hola " . $this->nombre . "</strong> Has creado tu cuenta en Dogtoon, solo debes confirmarla presionando en el siguiente enlace</p>";
-        $contenido.="<p>Presiona aqui: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
+        $contenido.="<p>Presiona aqui: <a href='https://safe-sea-39879.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confirmar Cuenta</a></p>";
         $contenido.="<p>Si tu no solicitaste esta cuenta, puedes ignorar este mensaje</p>";
         $contenido.="</html>";
         $mail->Body=$contenido;
@@ -64,7 +64,7 @@ class Email{
 
         $contenido= "<html>";
         $contenido.= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado reestablecer tu contraseña, sigue el siguiente enlace para hacerlo.</p>";
-        $contenido.="<p>Presiona aqui: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer Contraseña</a></p>";
+        $contenido.="<p>Presiona aqui: <a href='https://safe-sea-39879.herokuapp.com/recuperar?token=" . $this->token . "'>Reestablecer Contraseña</a></p>";
         $contenido.="<p>Si tu no solicitaste este cambio, puedes ignorar este mensaje</p>";
         $contenido.="</html>";
         $mail->Body=$contenido;
